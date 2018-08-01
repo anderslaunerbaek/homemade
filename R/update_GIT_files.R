84fb20e3d9242881b7550b3119d16b4d4bb29245
Anders Launer Bæk <anders@launer.dk>
2018-08-01 11:08:45 +0200

    good functions

---
+++ R/update_GIT_files.R
@@ -0,0 +1,40 @@
+#' update_GIT_files
+#'
+#' @param commit_msg msg which describes the commit.
+#' @param path directory to the GIT repo.
+#' @param files files you want to update.
+#'
+#' @return NULL.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   update_GIT_files(commit_msg = "test", path = "~/Downloads/test/", files = NULL)
+#' }
+#'
+update_GIT_files <- function(commit_msg, path, files = NULL) {
+  #
+  recent_wd <- getwd()
+  setwd(path)
+
+  if (is.null(files)) {
+    files <- "-A"
+  } else {
+    files <- paste0(files, collapse = " ")
+  }
+
+  # git status
+  system("git status")
+  # git add
+  system(paste("git add", files))
+  # git commit
+  system(paste("git commit -m'", commit_msg, "'"))
+  # git push
+  system("git push")
+
+  # get back to the normal wd agian
+  setwd(recent_wd)
+
+  #
+  return(invisible(NULL))
+}
